fca37472c38d0f0a209ef68c86492b2613bb34ac
sofinde <soeren.findeisen@mercedes-benz.com>
2022-09-19 22:00:50 +0200

    initial renderer

--- WeekendRaytracer/src/WeekendRaytracer.cpp
+++ WeekendRaytracer/src/WeekendRaytracer.cpp
@@ -4,39 +4,55 @@
 #include "Walnut/Application.h"
 #include "Walnut/EntryPoint.h"
 #include "Walnut/Image.h"
+#include "Walnut/Random.h"
+#include "Walnut/Timer.h"
 
 using namespace Walnut;
 
-class ExampleLayer : public Walnut::Layer {
-    public:
-    virtual void OnUIRender () override {
-        ImGui::Begin ("Settings");
-        if (ImGui::Button ("Render")) {
-            Render ();
+class ExampleLayer : public Walnut::Layer
+{
+  public:
+    virtual void OnUIRender() override
+    {
+        ImGui::Begin("Settings");
+        ImGui::Text("Last Render: %.3fms", lastRenderTime_);
+        if (ImGui::Button("Render"))
+        {
+            Render();
         }
-        ImGui::End ();
+        ImGui::End();
 
-        ImGui::Begin ("Viewport");
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+        ImGui::Begin("Viewport");
 
-        viewportWidth_  = ImGui::GetContentRegionAvail ().x;
-        viewportHeight_ = ImGui::GetContentRegionAvail ().y;
+        viewportWidth_ = ImGui::GetContentRegionAvail().x;
+        viewportHeight_ = ImGui::GetContentRegionAvail().y;
 
         if (image_)
-            ImGui::Image (image_->GetDescriptorSet (), { (float)image_->GetWidth (), (float)image_->GetHeight () });
+            ImGui::Image(image_->GetDescriptorSet(), { (float)image_->GetWidth(), (float)image_->GetHeight() });
 
-        ImGui::End ();
+        ImGui::End();
+        ImGui::PopStyleVar();
+
+        Render();
     }
 
-    void Render () {
-        if (!image_ || viewportHeight_ != image_->GetWidth () || viewportWidth_ != image_->GetWidth ()) {
-            image_ = std::make_shared<Image> (viewportWidth_, viewportHeight_, ImageFormat::RGBA);
+    void Render()
+    {
+        timer_.Reset();
+        if (!image_ || viewportHeight_ != image_->GetWidth() || viewportWidth_ != image_->GetWidth())
+        {
+            image_ = std::make_shared<Image>(viewportWidth_, viewportHeight_, ImageFormat::RGBA);
             delete[] imageData_;
             imageData_ = new uint32_t[viewportWidth_ * viewportHeight_];
 
-            for (uint32_t i = 0; i < viewportWidth_ * viewportHeight_; i++) {
-                imageData_[i] = 0xffff00ff;
+            for (uint32_t i = 0; i < viewportWidth_ * viewportHeight_; i++)
+            {
+                imageData_[i] = Random::UInt();
+                imageData_[i] |= 0xff000000;
             }
-            image_->SetData (imageData_);
+            image_->SetData(imageData_);
+            lastRenderTime_ = timer_.ElapsedMillis();
         }
     }
 
@@ -45,20 +61,26 @@ class ExampleLayer : public Walnut::Layer {
 
     std::shared_ptr<Walnut::Image> image_;
     uint32_t* imageData_ = nullptr;
+
+    Timer timer_;
+    float lastRenderTime_{ 0 };
 };
 
-Walnut::Application* Walnut::CreateApplication (int argc, char** argv) {
+Walnut::Application* Walnut::CreateApplication(int argc, char** argv)
+{
     Walnut::ApplicationSpecification spec;
     spec.Name = "Weekend Raytracer";
 
-    Walnut::Application* app = new Walnut::Application (spec);
-    app->PushLayer<ExampleLayer> ();
-    app->SetMenubarCallback ([app] () {
-        if (ImGui::BeginMenu ("File")) {
-            if (ImGui::MenuItem ("Exit")) {
-                app->Close ();
+    Walnut::Application* app = new Walnut::Application(spec);
+    app->PushLayer<ExampleLayer>();
+    app->SetMenubarCallback([app]() {
+        if (ImGui::BeginMenu("File"))
+        {
+            if (ImGui::MenuItem("Exit"))
+            {
+                app->Close();
             }
-            ImGui::EndMenu ();
+            ImGui::EndMenu();
         }
     });
     return app;
