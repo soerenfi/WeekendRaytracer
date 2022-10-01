311701807b9459cf4c99d845e2cbdb7703c35645
sofinde <soeren.findeisen@mercedes-benz.com>
2022-10-01 08:02:24 +0200

    add material

--- WeekendRaytracer/src/Scene.h
+++ WeekendRaytracer/src/Scene.h
@@ -3,12 +3,22 @@
 #include <glm/glm.hpp>
 #include <vector>
 
+struct Material
+{
+    glm::vec3 albedo{ 1.0f };
+    float specular{ 1.0f };
+};
+
 struct Sphere
 {
     glm::vec3 position{ 0.0f };
     float radius = 0.5f;
 
-    glm::vec3 albedo{ 1.0f };
+    Material material;
+
+    bool RayIntersect(const glm::vec3& origin, const glm::vec3& direction, float firstHit)
+    {
+    }
 };
 
 struct Scene
