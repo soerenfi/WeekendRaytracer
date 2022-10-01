98f468167726713dc0b9633cdb456e8f3680be58
sofinde <soeren.findeisen@mercedes-benz.com>
2022-10-01 08:02:16 +0200

    add ray payload

--- WeekendRaytracer/src/Ray.h
+++ WeekendRaytracer/src/Ray.h
@@ -2,8 +2,19 @@
 
 #include <glm/glm.hpp>
 
+struct RayPayload
+{
+    float hitDistance = std::numeric_limits<float>::max();
+    glm::vec3 hitPosition;
+    glm::vec3 hitNormal;
+    glm::vec3 color = glm::vec3(0.0f);
+    uint32_t objectIndex;
+    uint16_t traceDepth = 0;
+};
+
 struct Ray
 {
     glm::vec3 Origin;
     glm::vec3 Direction;
+    RayPayload payload;
 };
